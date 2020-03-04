c00a36fbba690da980283e50a233e5cca8ef973c
mayberrs <scott.mayberry@vumc.org>
2020-03-04 09:33:30 -0600

    Improve test case

--- src/test/java/com/berryworks/jquantify/SessionCounterTest.java
+++ src/test/java/com/berryworks/jquantify/SessionCounterTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 public class SessionCounterTest {
     public static final double DELTA = 0.0001;
+    public static final int BIG_DELTA = 10;
     SessionCounter mCounter;
 
     @Before
@@ -166,7 +167,6 @@ public class SessionCounterTest {
         //  +140  stop (assumed to be E)
 
 
-        long time_0 = Clock.now();
         mCounter.start(); // A
         assertEquals(0, mCounter.getMaximumSessionTime());
         assertEquals(0, mCounter.getSessionTime(), DELTA);
@@ -179,58 +179,65 @@ public class SessionCounterTest {
         assertEquals(0.0, mCounter.getSessionTimeMean(), 0.00);
 
         Thread.sleep(10);
+        // time is now +20
         mCounter.start(); // C
         assertEquals(0, mCounter.getMaximumSessionTime());
         assertEquals(0, mCounter.getSessionTime(), DELTA);
         assertEquals(0.0, mCounter.getSessionTimeMean(), 0.00);
 
         Thread.sleep(20);
+        // time is now +40
         mCounter.stop(); // A
-        assertEquals(40, mCounter.getMaximumSessionTime(), 5);
-        assertEquals(40, mCounter.getSessionTime(), 5);
-        assertEquals(40.0, mCounter.getSessionTimeMean(), 5.00);
+        // The session times reported should reflect the one session that has completed
+        assertEquals(40, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(40, mCounter.getSessionTime(), BIG_DELTA);
+        assertEquals(40.0, mCounter.getSessionTimeMean(), 10.00);
 
         Thread.sleep(20);
+        // time is now +60
         mCounter.stop(); // B
-//        assertEquals(50, mCounter.getMaximumSessionTime(), 5);
-//        assertEquals(90, mCounter.getSessionTime(), 5);
-//        assertEquals((40 + 50) / 2.0, mCounter.getSessionTimeMean(), 5.00);
+        // We have 2 completed sessions, one of duration 40 and the other of duration 50
+        assertEquals(50, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(90, mCounter.getSessionTime(), 2 * BIG_DELTA);
+        assertEquals((40 + 50) / 2.0, mCounter.getSessionTimeMean(), 10.00);
 
         Thread.sleep(10);
+        // time is now +70
         mCounter.start(); // D
-        assertEquals(50, mCounter.getMaximumSessionTime(), 5);
-//        assertEquals(90, mCounter.getSessionTime(), 5);
-        assertEquals((40 + 50) / 2.0, mCounter.getSessionTimeMean(), 5.00);
+        assertEquals(50, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(90, mCounter.getSessionTime(), 2 * BIG_DELTA);
+        assertEquals((40 + 50) / 2.0, mCounter.getSessionTimeMean(), BIG_DELTA);
 
         Thread.sleep(10);
+        // time is now +80
         mCounter.stop(); // C
-//        assertEquals(60, mCounter.getMaximumSessionTime(), 5);
-//        assertEquals(150, mCounter.getSessionTime(), 5);
-        assertEquals((40 + 50 + 60) / 3.0, mCounter.getSessionTimeMean(), 5.00);
+        assertEquals(60, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(150, mCounter.getSessionTime(), 3 * BIG_DELTA);
+        assertEquals((40 + 50 + 60) / 3.0, mCounter.getSessionTimeMean(), BIG_DELTA);
 
         Thread.sleep(20);
         mCounter.stop(); // D
-        assertEquals(60, mCounter.getMaximumSessionTime(), 5);
-//        assertEquals(180, mCounter.getSessionTime(), 5);
-        assertEquals((40 + 50 + 60 + 30) / 4.0, mCounter.getSessionTimeMean(), 5.00);
+        assertEquals(60, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(180, mCounter.getSessionTime(), 4 * BIG_DELTA);
+        assertEquals((40 + 50 + 60 + 30) / 4.0, mCounter.getSessionTimeMean(), BIG_DELTA);
 
         Thread.sleep(10);
         mCounter.stop(); // ignored
-        assertEquals(60, mCounter.getMaximumSessionTime(), 5);
-//        assertEquals(180, mCounter.getSessionTime(), 5);
-//        assertEquals((40 + 50 + 60 + 30) / 4.0, mCounter.getSessionTimeMean(), 5.00);
+        assertEquals(60, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(180, mCounter.getSessionTime(), 4 * BIG_DELTA);
+        assertEquals((40 + 50 + 60 + 30) / 4.0, mCounter.getSessionTimeMean(), BIG_DELTA);
 
         Thread.sleep(20);
         mCounter.start(); // E
-        assertEquals(60, mCounter.getMaximumSessionTime(), 5);
-//        assertEquals(180, mCounter.getSessionTime(), 5);
-        assertEquals((40 + 50 + 60 + 30) / 4.0, mCounter.getSessionTimeMean(), 5.00);
+        assertEquals(60, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(180, mCounter.getSessionTime(), 4 * BIG_DELTA);
+        assertEquals((40 + 50 + 60 + 30) / 4.0, mCounter.getSessionTimeMean(), BIG_DELTA);
 
         Thread.sleep(10);
         mCounter.stop(); // E
-        assertEquals(60, mCounter.getMaximumSessionTime(), 5);
-//        assertEquals(190, mCounter.getSessionTime(), 5);
-        assertEquals((40 + 50 + 60 + 30 + 10) / 5.0, mCounter.getSessionTimeMean(), 5.00);
+        assertEquals(60, mCounter.getMaximumSessionTime(), BIG_DELTA);
+        assertEquals(190, mCounter.getSessionTime(), 5 * BIG_DELTA);
+        assertEquals((40 + 50 + 60 + 30 + 10) / 5.0, mCounter.getSessionTimeMean(), BIG_DELTA);
     }
 
     @Test
